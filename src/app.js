8b031c659b5e3040248988692c7d5141fb3676ae
prritam <manepritam81@gmail.com>
2025-07-27 14:08:01 +0000

    first push to main

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from OpenShift Node.js app!  i changes this and deployment is automated ");
+  res.send("Hello from OpenShift Node.js app!  i changes this and deployment is automated|| after push if woekflows is successful you can see this in browser ");
 });
 
 app.listen(PORT, () => {
